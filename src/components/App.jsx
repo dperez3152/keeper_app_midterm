88b23a8b3c37ec8da2e412fb8665b7bf69811a4a
dperez3152 <78930169+dperez3152@users.noreply.github.com>
2023-12-17 04:13:00 +0000

    fixing addNote

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -71,7 +71,7 @@ function App() {
       })
       .then(data => {
         setNotes((prevNotes) => {
-          return [...prevNotes, { _id: data.insertedId, title: {inputTitle}, content: {inputContent}}];
+          return [...prevNotes, { _id: data.insertedId, title: inputTitle, content: inputContent}];
         });
         //setNames(prevNames => [...prevNames, { _id: data.insertedId, name: newName }]);
       })
